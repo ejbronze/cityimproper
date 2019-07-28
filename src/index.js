ee5fdc610ff005be40288b91bc4bb59976210894
Edwin Jaquez <eddie@Edwins-iMac.fios-router.home>
2019-07-28 15:02:51 -0400

    practice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './counter';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const element = <h1>Hello World</h1>
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
