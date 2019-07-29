784ee944856bf62296edb579bedf9c84983a44d5
Edwin Jaquez <eddie@Edwins-iMac.fios-router.home>
2019-07-29 09:23:36 -0400

    no changes

--- src/counter.jsx
+++ src/counter.jsx
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 1
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     };
   
     render() { 
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-
         return  (
         <div
-            ><span className={classes}>{this.formatCount()}</span>
+            ><span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                { this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }
+            </ul>
         </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
