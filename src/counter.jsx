953c1a8ea62eb27f65ab368f3972d5d1956d2a20
Edwin Jaquez <eddie@Edwins-iMac.fios-router.home>
2019-07-28 21:25:51 -0400

    working with classes

--- src/counter.jsx
+++ src/counter.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 1
     };
-    
+  
     render() { 
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+
         return  (
         <div
-            ><span className="badge badge-primary m-2">{this.formatCount()}</span>
+            ><span className={classes}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
