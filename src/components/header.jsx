77b857c737cce8d902e1f13e0852458f8d0e86dc
Edwin Jaquez <edsmacbook@gmail.com>
2019-07-31 11:31:54 -0400

    updated the background image on the banner

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
+import Background from "../images/bronx.jpg";
+
+var headerStyle = {
+	backgroundImage: `url(${Background})`,
+	backgroundSize: "cover",
+	overflow: "hidden"
+};
 
 class Header extends Component {
 	render() {
 		return (
 			<div>
-				<header class="bg-primary py-5 mb-5">
-					<div class="container h-100">
-						<div class="row h-100 align-items-center">
-							<div class="col-lg-12">
-								<h1 class="display-4 text-white mt-5 mb-2">City Improper</h1>
-								<p class="lead mb-5 text-white-50">
+				<header style={headerStyle} className="py-5 mb-5">
+					<div className="container h-100">
+						<div className="row h-100 align-items-center">
+							<div className="col-lg-12">
+								<h1 className="display-4 text-white mt-5 mb-2">
+									City Improper
+								</h1>
+								<p className="lead mb-5 text-white-50">
 									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non
 									possimus ab labore provident mollitia. Id assumenda voluptate
 									earum corporis facere quibusdam quisquam iste ipsa cumque unde
