7c1cf02633ac03dbe8dd0de39b0aa3e105104945
Eddie Jaquez <edsmacbook@gmail.com>
2019-08-01 23:53:09 -0400

    no changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  FormGroup
+} from "react-bootstrap";
 
 class Navibar extends Component {
-	render() {
-		return (
-			<div>
-				<Navbar bg="dark" variant="dark">
-					<Navbar.Brand href="#home">ICON GOES HERE</Navbar.Brand>
-					<Nav className="mr-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#Boro">Boro</Nav.Link>
-					</Nav>
-					<Form inline>
-						<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-						<Button variant="outline-secondary">Search</Button>
-					</Form>
-				</Navbar>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">ICON GOES HERE</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#Boro">Boro</Nav.Link>
+          </Nav>
+          <Form inline>
+            {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" /> */}
+            <Button variant="outline-light">Log In</Button>
+          </Form>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default Navibar;
--- src/components/trending.jsx
+++ src/components/trending.jsx
@@ -1,81 +1,82 @@
 import React, { Component } from "react";
+//add scraper function
 
 class Trending extends Component {
-	render() {
-		return (
-			<div>
-				{/* <!-- Page Content --> */}
-				<div className="container">
-					<div className="row">
-						<div className="col-md-4 mb-5">
-							<div className="card h-100">
-								<img
-									className="card-img-top"
-									src="http://placehold.it/300x200"
-									alt=""
-								/>
-								<div className="card-body">
-									<h4 className="card-title">Card title</h4>
-									<p className="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Sapiente esse necessitatibus neque sequi doloribus.
-									</p>
-								</div>
-								<div className="card-footer">
-									<a href="#" className="btn btn-primary">
-										Find Out More!
-									</a>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-4 mb-5">
-							<div className="card h-100">
-								<img
-									className="card-img-top"
-									src="http://placehold.it/300x200"
-									alt=""
-								/>
-								<div className="card-body">
-									<h4 className="card-title">Card title</h4>
-									<p className="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Sapiente esse necessitatibus neque sequi doloribus totam ut
-										praesentium aut.
-									</p>
-								</div>
-								<div className="card-footer">
-									<a href="#" className="btn btn-primary">
-										Find Out More!
-									</a>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-4 mb-5">
-							<div className="card h-100">
-								<img
-									className="card-img-top"
-									src="http://placehold.it/300x200"
-									alt=""
-								/>
-								<div className="card-body">
-									<h4 className="card-title">Card title</h4>
-									<p className="card-text">
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-										Sapiente esse necessitatibus neque.
-									</p>
-								</div>
-								<div className="card-footer">
-									<a href="#" className="btn btn-primary">
-										Find Out More!
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {/* <!-- Page Content --> */}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="http://placehold.it/300x200"
+                  alt=""
+                />
+                <div className="card-body">
+                  <h4 className="card-title">Card title</h4>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Sapiente esse necessitatibus neque sequi doloribus.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="http://placehold.it/300x200"
+                  alt=""
+                />
+                <div className="card-body">
+                  <h4 className="card-title">Card title</h4>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Sapiente esse necessitatibus neque sequi doloribus totam ut
+                    praesentium aut.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="http://placehold.it/300x200"
+                  alt=""
+                />
+                <div className="card-body">
+                  <h4 className="card-title">Card title</h4>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Sapiente esse necessitatibus neque.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a href="#" className="btn btn-primary">
+                    Find Out More!
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Trending;
