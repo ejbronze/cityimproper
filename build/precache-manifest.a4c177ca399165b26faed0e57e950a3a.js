5ba8e7ae1b1b6f248a894b27bdf0a8599f77f75e
Edwin Jaquez <edsmacbook@gmail.com>
2019-08-05 05:04:01 -0400

    undid firebase hosting for now

---
+++ build/precache-manifest.a4c177ca399165b26faed0e57e950a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b73ccd210172420a6b6dda1d7fe7f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8535c624b8de0bbe9497",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8535c624b8de0bbe9497",
+    "url": "/static/js/2.f5253a24.chunk.js"
+  },
+  {
+    "revision": "f9153fd0bd763e271850",
+    "url": "/static/js/main.c9ee5043.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "91c0e6d19e38087f944dadca31378520",
+    "url": "/static/media/bronx.91c0e6d1.jpg"
+  },
+  {
+    "revision": "e290ce43ff75996d0fd3f70e222231ae",
+    "url": "/static/media/newlogo.e290ce43.png"
+  }
+]);
